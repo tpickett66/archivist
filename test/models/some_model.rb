ce78de2c348d51b263005e416ac0d956b67b3c40
t.pickett66 <t.pickett66@gmail.com>
2011-02-05 00:10:26 -0600

    add an instance method to our test model

--- test/models/some_model.rb
+++ test/models/some_model.rb
@@ -1,6 +1,9 @@
 class SomeModel < ActiveRecord::Base
-  # acts_as_archive
   serialize(:random_array,Array)
   serialize(:some_hash,Hash)
   has_archive :indexes=>[:first_name,:last_name]
+
+  def full_name
+    "#{last_name}, #{first_name}"
+  end
 end
